59b4eebcb0625135c01585b0c40a1a0b72c00752
marcuswesterblom <marcus.vesterblom@gmail.com>
2026-02-04 17:21:09 +0100

    Change project data

--- src/utils/projectsData.ts
+++ src/utils/projectsData.ts
@@ -4,22 +4,15 @@ export const projects = [
     new ProjectCard(
         "Webshop",
         "A group project built as part of a Frontend Development course. I was responsible for the entire cart and checkout functionality, including state management with localStorage, adding and removing products, and implementing the checkout flow using HTML, SCSS, and TypeScript. The visual design was provided as part of the assignment.",
-        ["TypeScript", "SCSS", "Vite", "HTML"],
-        "https://marcuswesterblom-webshop.netlify.app/",
+        ["TypeScript", "SCSS", "Vite", "HTML", "Agile Project Management", "School project"],
+        "https://marcurwesterblom-webshop.netlify.app/",
         "https://github.com/marcuswesterblom/webshop_fed25"
     ),
     new ProjectCard(
         "Todo-list",
         "A frontend application built during the Frontend Developer program. It allows users to manage tasks by adding, completing, and removing todos, with a focus on JavaScript fundamentals and dynamic UI updates.",
-        ["JavaScript", "SCSS", "Vite", "HTML"],
+        ["JavaScript", "SCSS", "Vite", "HTML", "Tailwind", "School project"],
         "https://marcuswesterblom-todolist.netlify.app/",
         "https://github.com/marcuswesterblom/todo_list_fed25"
-    ),
-    new ProjectCard(
-        "Portfolio",
-        "A quick description of my project",
-        ["TypeScript", "SCSS", "Vite", "HTML"],
-        "https://exampel.com",
-        "https://github.com/marcuswesterblom/my-portfolio.git"
-    ),
+    )
 ];
